8c0bedb8ce6717501d9a8db2cc6428ab63cdd227
cadufunside <114048341+cadufunside@users.noreply.github.com>
2025-11-09 00:51:14 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    // se quiser menos barulho de log: logger: ['error', 'warn', 'log'],
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // CORS liberado pro front/Socket.IO
   app.enableCors({ origin: true, credentials: true });
-
-  // WebSocket/Socket.IO no mesmo servidor HTTP
   app.useWebSocketAdapter(new IoAdapter(app));
-
-  // Pipes globais (opcional, mas recomendado)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  // Porta/host: Railway precisa de 0.0.0.0
   const port = parseInt(process.env.PORT || '3001', 10);
   await app.listen(port, '0.0.0.0');
 
